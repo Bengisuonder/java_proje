b52353442e83596d71df12c7fd0c86712b4bde0d
Bengisu <bengisu.161@gmail.com>
2025-05-24 12:30:39 +0300

    oparatörler

---
+++ operatorler.java
@@ -0,0 +1,15 @@
+public class operatorler {
+    public static void main(String[] args)
+    {
+        int sayi1= 21;
+        int sayi2= 16;
+        int toplam= sayi1 + sayi2;
+        int cikarma= sayi1 - sayi2;
+        int carpma= sayi1 * sayi2;
+        int bolme= sayi1 / sayi2;
+        System.out.println("Toplama işleminin sonucu: "+ toplam);
+        System.out.println("Çıkarma işleminin sonucu: "+ cikarma);
+        System.out.println("Çarpma işleminin sonucu: "+ carpma);
+        System.out.println("Bölme işleminin sonucu: "+ bolme);
+    }
+}
