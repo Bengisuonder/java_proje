75fd44779f3a5785765e8144f6766a84efc2f598
Bengisu <bengisu.161@gmail.com>
2025-05-24 11:28:31 +0300

    veri tipleri

---
+++ degiskenler.java
@@ -0,0 +1,24 @@
+public class degiskenler {
+    public static void main(String[]args)
+    {
+        byte a=112;
+        short b=32767;
+        int c=1;
+        long d=-922337203685477580L;
+        double e=4.9;
+        float f=21.7f;
+        String isim="Bengisu";
+        char cinsiyet='K';
+        boolean isTrue=true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(isim + " + " + cinsiyet);
+        System.out.println(isTrue);
+
+    }
+}
