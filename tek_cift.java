9d03c8d2f3607771c17aede3481bbef973268be2
Bengisu <bengisu.161@gmail.com>
2025-05-24 13:02:48 +0300

    tek-çift

---
+++ tek_cift.java
@@ -0,0 +1,15 @@
+public class tek_cift {
+    public static void main(String[]args)
+    {
+        int sayi=54;
+
+        if(sayi%2==0)
+        {
+            System.out.println("Çift sayı.");
+        }
+        else
+        {
+            System.out.println("Tek sayı.");
+        }
+    }
+}
