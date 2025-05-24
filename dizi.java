cc8422b034a150641eaf116ddcac99126fb9c9e0
Bengisu <bengisu.161@gmail.com>
2025-05-24 14:38:31 +0300

    dizi

---
+++ dizi.java
@@ -0,0 +1,10 @@
+public class dizi {
+    public static void main(String[]args)
+    {
+        int[] dizi= {1,2,3,4,5};
+        for(int i=1;i<= dizi.length;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
